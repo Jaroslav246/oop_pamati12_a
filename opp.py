72c4b491ce53614065aeb1f4b8eaf28bebf1c942
Jaroslav246 <slav4nko@gmail.com>
2024-09-16 09:12:27 +0300

    Create opp.py

---
+++ opp.py
@@ -0,0 +1,20 @@
+class person:
+    def set(self,name,age):
+        self.name = name 
+        self.age = age 
+    def output(self):
+        print(self.name,self.age)
+    def labdien(self):
+        print(f'Labdien, {self.name}!')
+
+p = person()
+p.set("Peter",20)
+d=person()
+d.set("Dzon",24)
+p.output()
+p.labdien()
+d.output()
+d.labdien()
+
+print("Peter",20)
+int(input('Labdien, Peter!'))
