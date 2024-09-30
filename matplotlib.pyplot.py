669046f4b88a845ac76979bcf3798780fcdb4976
Jaroslav246 <slav4nko@gmail.com>
2024-09-30 09:10:55 +0300

    Create matplotlib.pyplot.py

---
+++ matplotlib.pyplot.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+
+# Veidojam vienkāršu grafiku
+plt.plot([1, 2, 3, 4])
+plt.ylabel('Funkcijas grafiks')
+plt.show()
+
+plt.plot([4, 7, 2, 16,3])
+plt.ylabel('Funkcija')
+plt.show()
